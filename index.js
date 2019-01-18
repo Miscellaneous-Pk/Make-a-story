2496dbc54ee25756029135ed227f36065b467864
Qasim Ali <qasimali24@gmail.com>
2019-01-18 11:02:43 +0500

    at HQ CC

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const _ = require('lodash');
 
+const {mongoose} = require('./db/mongoose');
+
 
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 app.use(express.static(__dirname+'/static'));
 app.use(bodyParser.json());
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 
 app.get('/',(req,res) => {
-  res.render('index.hbs');
+  res.render('register.hbs');
 })
 
 // serverRunning();
---
+++ models/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var usersSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+Users.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+
+module.exports = {users};
