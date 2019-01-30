161d06bb21d69b94f37413fc20bf3fb32cd62705
Qasim <qasimali24@gmail.com>
2019-01-30 22:59:50 +0500

    Mongoose and mongo environment is now working. Now bring user's name email and password from .hbs to database.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const _ = require('lodash');
 
+const {mongoose} = require('./db/mongoose');
+const {Users} = require('./models/users');
 
 var app = express();
 var port = process.env.PORT;
@@ -13,12 +15,25 @@ app.use(bodyParser.json());
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 
+
 app.get('/',(req,res) => {
   res.render('index.hbs');
-})
-
-// serverRunning();
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 })
+
+
+
+// var user = new Users({
+//   "name": "Qasim",
+//   "email":"qasimali24@gmail.com",
+//   "password":"1234"
+// })
+//
+// user.save().then((returned) => {
+//   return console.log('saved', returned.name);
+// }).catch((e) => {
+//   return console.log('there was an error',e);
+// })
---
+++ models/users.js
@@ -0,0 +1,23 @@
+const {mongoose} = require('../db/mongoose');
+
+var UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+});
+
+var Users = mongoose.model('Users', UsersSchema);
+
+module.exports = {Users};
