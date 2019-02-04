939edf86f1b5bf1a92bf4626d963964d99e21548
Qasim <qasimali24@gmail.com>
2019-02-04 19:17:52 +0500

    token generation in progress

--- models/users.js
+++ models/users.js
@@ -1,4 +1,6 @@
-const {mongoose} = require('../db/mongoose');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UsersSchema = new mongoose.Schema({
   name: {
@@ -15,8 +17,73 @@ var UsersSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    }
+  }]
+});
+
+UsersSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
 });
+
+UsersSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.updateOne({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+UsersSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return user;
+  });
+};
+
+UsersSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 
 var Users = mongoose.model('Users', UsersSchema);
 
